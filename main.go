63164b1a8048c38766736f1774af65f89fcb980f
Excel18-coder <excel2130@gmail.com>
2026-03-28 20:25:18 -0400

    added the update movie function

--- main.go
+++ main.go
@@ -4,21 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type Movie struct {
-	ID       string    `json::"id`
+	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title`
-	Director *Director `json:"director`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
 }
 
 type Director struct {
-	firstName string `json:"firstname"`
-	lastName  string `json:"lastname"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
 }
 
 var movies []Movie
@@ -56,24 +58,43 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createMovie(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
-	=json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.itoa(rand.intn(1000000000))
+
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000000))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+
+		}
+
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	movies = append(movies, Movie{ID: "1", Isbn: " 123", Title: "Movie one", Director: &Director{firstName: "Excel", lastName: "Baraka"}})
-	movies = append(movies, Movie{ID: "2", Isbn: " 124", Title: "Movie Two", Director: &Director{firstName: "Fav", lastName: "Neema"}})
-	movies = append(movies, Movie{ID: "3", Isbn: " 125", Title: "Movie Three", Director: &Director{firstName: "Faith", lastName: "Natasha"}})
-	movies = append(movies, Movie{ID: "4", Isbn: " 126", Title: "Movie Four", Director: &Director{firstName: "Ian", lastName: "Dior"}})
-	movies = append(movies, Movie{ID: "5", Isbn: " 127", Title: "Movie Five", Director: &Director{firstName: "Peter", lastName: "Thiel"}})
+	movies = append(movies, Movie{ID: "1", Isbn: " 123", Title: "Movie one", Director: &Director{FirstName: "Excel", LastName: "Baraka"}})
+	movies = append(movies, Movie{ID: "2", Isbn: " 124", Title: "Movie Two", Director: &Director{FirstName: "Fav", LastName: "Neema"}})
+	movies = append(movies, Movie{ID: "3", Isbn: " 125", Title: "Movie Three", Director: &Director{FirstName: "Faith", LastName: "Natasha"}})
+	movies = append(movies, Movie{ID: "4", Isbn: " 126", Title: "Movie Four", Director: &Director{FirstName: "Ian", LastName: "Dior"}})
+	movies = append(movies, Movie{ID: "5", Isbn: " 127", Title: "Movie Five", Director: &Director{FirstName: "Peter", LastName: "Thiel"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
