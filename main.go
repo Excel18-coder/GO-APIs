b3521feca9afabac35245669a33f368c34238f64
Excel18-coder <excel2130@gmail.com>
2026-04-02 13:00:07 -0400

    marshalling

--- main.go
+++ main.go
@@ -3,106 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-	"math/rand"
-	"net/http"
-	"strconv"
-
-	"github.com/gorilla/mux"
+	"os"
 )
 
-type Movie struct {
-	ID       string    `json:"id"`
-	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title"`
-	Director *Director `json:"director"`
-}
-
-type Director struct {
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-}
-
-var movies []Movie
-
-func getMovies(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
-
-}
-
-func deleteMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range movies {
-
-		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
-			break
-		}
-
-	}
-	json.NewEncoder(w).Encode(movies)
-
-}
-
-func getMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-
-	for _, item := range movies {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-}
-
-func createMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var movie Movie
-
-	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.Itoa(rand.Intn(1000000000))
-	movies = append(movies, movie)
-	json.NewEncoder(w).Encode(movie)
-
-}
-
-func updateMovie(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-
-	for index, item := range movies {
-		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
-			var movie Movie
-			_ = json.NewDecoder(r.Body).Decode(&movie)
-			movie.ID = params["id"]
-			movies = append(movies, movie)
-			json.NewEncoder(w).Encode(movie)
-
-		}
-
-	}
+type Person struct {
+	Name   string `json:"username"`
+	Age    int    `json:"age"`
+	Active bool   `json:"is_active"`
 }
 
 func main() {
-	r := mux.NewRouter()
+	p := Person{
+		Name:   "Excel",
+		Age:    20,
+		Active: true,
+	}
 
-	movies = append(movies, Movie{ID: "1", Isbn: " 123", Title: "Movie one", Director: &Director{FirstName: "Excel", LastName: "Baraka"}})
-	movies = append(movies, Movie{ID: "2", Isbn: " 124", Title: "Movie Two", Director: &Director{FirstName: "Fav", LastName: "Neema"}})
-	movies = append(movies, Movie{ID: "3", Isbn: " 125", Title: "Movie Three", Director: &Director{FirstName: "Faith", LastName: "Natasha"}})
-	movies = append(movies, Movie{ID: "4", Isbn: " 126", Title: "Movie Four", Director: &Director{FirstName: "Ian", LastName: "Dior"}})
-	movies = append(movies, Movie{ID: "5", Isbn: " 127", Title: "Movie Five", Director: &Director{FirstName: "Peter", LastName: "Thiel"}})
+	f, err := os.Create("output.json")
+	if err != nil {
+		fmt.Println("error creating file", err)
+		panic(err)
+	}
+	defer f.Close()
 
-	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
-
-	fmt.Printf("Start the server at port 8000\n")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	encoder := json.NewEncoder(f)
+	err = encoder.Encode(p)
+	if err != nil {
+		fmt.Println("error encoding person: ", err)
+		panic(err)
+	}
 
 }
